5dfc9caaed701730b7e8fee29eb9dbf92834db13
CindyMisoi <crmisoi2k@gmail.com>
2023-08-15 23:24:37 +0300

    updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-<App />
-);
+ReactDOM.render(<App />, document.getElementById("root"));
