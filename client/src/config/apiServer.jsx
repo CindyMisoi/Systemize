279bc483e39fd58cb91f88aeb30529fca50d1184
CindyMisoi <crmisoi2k@gmail.com>
2023-08-15 22:29:11 +0300

    fixed peer dependency issue

--- client/src/config/apiServer.jsx
+++ client/src/config/apiServer.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const sessionToken = sessionStorage.getItem("session_token"); // Replace with the actual session token value
 
 const apiServer = axios.create({
-  baseURL: "http://localhost:3000", // Replace with your Rails API server URL
+  baseURL: "https://systemize-api.onrender.com", // Replace with your Rails API server URL
   withCredentials: true,
 });
 
